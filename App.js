76d337fa84da46ff8fe631daaadaae184c7708e1
Madhuwantstocode <148116807+Madhuwantstocode@users.noreply.github.com>
2024-01-08 15:11:26 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginsignup from './Components/Login-signup/Login-signup';
+
+function App() {
+  return (
+    <div>
+      <Loginsignup/>
+      
+    </div>
+  );
+}
+
+export default App;
